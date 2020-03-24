aae32bbded3c168db967ba21e496cb81b8337639
NoobieDEV <monthawit.milky@gmail.com>
2020-03-24 11:06:53 +0700

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -11,6 +11,15 @@ if ($connection->connect_errno) {
     exit();
 } else {
     printf("Connected to the database");
+   
+    $sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+    
 }
 $connection->close();
 ?>
